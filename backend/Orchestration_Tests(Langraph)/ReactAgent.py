772b8641b8c1c3b64c83a0f5ee742a12c291e39e
Santiago Olivera Vaquero <santiago.oliverav@gmail.com>
2026-05-15 20:12:23 +0200

    Organized_for_repo_folder_(Uses_Langraph). Needs to import tools and LLM API

--- backend/Orchestration_Tests(Langraph)/ReactAgent.py
+++ backend/Orchestration_Tests(Langraph)/ReactAgent.py
@@ -107,28 +107,3 @@ def should_continue(state: AgentMemory) -> Literal["tool_node", END]:
         return "tool_node"
     
     return END
-
-
-
-
-
-
-
-
-"""Data structure proposal for storing numeric data:
-dict[entity, dict[metric,dict[date, value]]]
-Repeat for many entities, each entity with many metrics, each metric with many dates"""
-raw_data: dict[str, dict[str, dict[date, float]]] = {}
-calculated_data: dict[str, dict[str, dict[date, float]]] = {}
-
-"""Function to update numeric data ...
-Work in progress"""
-def merge_nested(existing: dict, new:dict):
-    for entity, metrics in new.items():
-        if entity not in existing:
-            existing[entity] = {}
-            for metric, dates in metrics.items():
-                if metric not in existing[entity]:
-                    existing[entity][metric] = {}
-                existing[entity][metric].update(dates)
-    return existing
---
+++ backend/src/backend/Agent/state.py
@@ -0,0 +1,23 @@
+#Replace with file where chat model is called
+#from llm import CHAT_MODEL
+
+from langchain.agents import create_agent
+from langchain.messages import AnyMessage
+from typing_extensions import TypedDict, Annotated
+import operator
+
+#Replace with files with tools
+#from tools import tools
+
+model_with_tools = CHAT_MODEL.bind_tools(tools)
+tools_by_name = {tool.name: tool for tool in tools}
+
+#Stores agent memory (This is the state, different)
+class AgentMemory(TypedDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+    llm_calls: int
+    max_llm_calls: int
+    context: str
+    response_type: str
+    #raw_data: Annotated[dict, merge_nested]
+    #calculated_data: Annotated[dict, merge_nested]
